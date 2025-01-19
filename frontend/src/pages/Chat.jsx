915a8637f7dd3e15545079268d921ced759aa83a
Shifin-Malik <shifinmalik46@gmail.com>
2025-01-19 10:56:17 -0800

    update

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -3,7 +3,7 @@ import BotDialog from '../components/BotDialog'
 
 function Chat() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='flex justify-center items-center min-h-screen '>
       <BotDialog />
     </div>
   )
